a3d4f5490d7a3dc33ebc0e9767b2d2e92fdc8ff4
jfajaime <jfajaime@hotmail.com>
2025-01-02 16:23:00 -0300

    creando y agregando proyecto a git foro hub

---
+++ src/main/java/foro_hub/challenge_Api/ChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package foro_hub.challenge_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foro_hub/challenge_Api/ChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package foro_hub.challenge_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
