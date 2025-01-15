653121e3e7d059dd903b7a791583353af6fefc5c
jfajaime <jfajaime@hotmail.com>
2025-01-15 19:15:39 -0300

    se agrega spring docs y swagger

--- src/main/java/foro_hub/challenge_Api/security/SecurityConfiguration.java
+++ src/main/java/foro_hub/challenge_Api/security/SecurityConfiguration.java
@@ -31,6 +31,8 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(HttpMethod.POST, "/login")
                         .permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**")
+                        .permitAll()
                         .anyRequest()
                         .authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
